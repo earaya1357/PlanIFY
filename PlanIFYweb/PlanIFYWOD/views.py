8fc4947dbd27a29171607cb7e7d05c81ef598742
earaya1357 <arayaej@gmail.com>
2023-07-04 19:18:21 -0700

    everyting

--- PlanIFYweb/PlanIFYWOD/views.py
+++ PlanIFYweb/PlanIFYWOD/views.py
@@ -1,42 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .forms import EventForm, CreateUserForm, UserAccountForm, EventName
+from .forms import EventForm, CreateUserForm, UserAccountForm, EventName, AffiliateSearch, FullEventForm, EventWorkoutForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .models import UserAccount, Event
+from .models import UserAccount, Event, EventWorkOut
 from django.contrib.auth.models import User
 
 
-@login_required(login_url='SignIn')
-def home(request):
-    usern = UserAccount.objects.filter(username=request.user).values()
-    events = Event.objects.filter(event_host_user=int(usern[0]['user_id_number'])).values()
-    form = EventName()
-    if request.method == 'POST':
-        form = EventName(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            print(data)
-
-    return render(request, 'PlanIFYweb/dashboard.html', {'pagename':'Home', 'hostevents': events, 'form':form})
-
-
-@login_required(login_url='SignIn')
-def createnewevent(request):
-    form = EventForm()
-
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-        if form.is_valid():
-            form.save()
-            
-            return redirect('Home')
-
-
-    return render(request, 'PlanIFYweb/newevent.html', {'pagename': 'New Event', 'form': form})
-
-
+#Registion page for new users to sign up for the site.
 def register(request):
     if request.user.is_authenticated:
         return redirect('Home')
@@ -57,28 +29,7 @@ def register(request):
         return render(request, 'PlanIFYweb/register.html', {'form': form})
 
 
-
-def usersignin(request):
-    if request.user.is_authenticated:
-        return redirect('Home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('Home')
-
-        return render(request, 'PlanIFYweb/signin.html')
-
-
-def usersignout(request):
-    logout(request)
-    return render(request, 'PlanIFYweb/signin.html')
-
-
-
+#Full registration page for new users
 def completeregistration(request, username):
     if request.user.is_authenticated:
         return redirect('Home')
@@ -103,3 +54,121 @@ def completeregistration(request, username):
             temp = user.username
             form = UserAccountForm(initial={'username': temp, 'first_name_user': user.first_name, 'last_name_user': user.last_name, 'email_user': user.email})
         return render(request, 'PlanIFYweb/finishregistration.html', {'form': form, 'username': username})
+    
+
+#Sign in page for all users
+def usersignin(request):
+    if request.user.is_authenticated:
+        return redirect('Home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('Home')
+
+        return render(request, 'PlanIFYweb/signin.html')
+
+#Sign out page for all users
+def usersignout(request):
+    logout(request)
+    return render(request, 'PlanIFYweb/signin.html')
+
+
+def checkstatus(request):
+    usern = UserAccount.objects.filter(username=request.user).values()
+    athlete_approved = usern[0]['athlete_user_approved']
+    host_approved = usern[0]['host_user_approved']
+    vendor_approved = usern[0]['vendor_user_approved']
+    return athlete_approved, host_approved, vendor_approved
+
+
+#Home page for users
+@login_required(login_url='SignIn')
+def home(request):
+    usern = UserAccount.objects.filter(username=request.user).values()
+    athlete, host, vendor = checkstatus(request)
+
+    
+    #Filter events if the user is an athlete and has signed up for events
+    if athlete:
+        athleteevents = Event.objects.filter(event_host_user=int(usern[0]['user_id_number'])).values()
+    else:
+        athleteevents = None
+
+    #Filter events if the user is a host and has created events
+    if host:
+        hostevents = Event.objects.filter(event_host_user=int(usern[0]['user_id_number'])).values()
+    else:
+        hostevents = None
+
+    #Filter events if the user is a vendor and has signed up to cater events
+    if vendor:
+        vendorevents = Event.objects.filter(event_host_user=int(usern[0]['user_id_number'])).values()
+    else:
+        vendorevents = None
+    
+    form1 = EventName()
+    form2 = AffiliateSearch()
+    #Listen for POST requests and determine which form is being submitted
+    if request.method == 'POST':
+        if 'name' in request.POST:
+            form1 = EventName(request.POST)
+            if form1.is_valid():
+                data = form1.cleaned_data['name']
+                
+                return redirect('EventDesign', data)
+        
+        elif 'affiliate_name' in request.POST:
+            form2 = AffiliateSearch(request.POST)
+            if form2.is_valid():
+                data = form2.cleaned_data['affiliate_name']
+                print(data)
+            
+
+    return render(request, 'PlanIFYweb/dashboard.html', {'pagename':'Home', 'hostevents': hostevents, 'athleteevents': athleteevents, 'vendorevents': vendorevents,'form1':form1, 'form2':form2})
+
+
+@login_required(login_url='SignIn')
+def eventdesign(request, eventname):
+    usern = UserAccount.objects.filter(username=request.user).values()[0]['user_id_number']
+    form = EventForm(initial={'event_host_user': usern,'event_name': eventname})
+
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+
+        if form.is_valid():
+            
+            form.save()
+            
+            return redirect('Home')
+
+
+    return render(request, 'PlanIFYweb/eventdesign.html', {'pagename': eventname, 'form': form})
+
+
+
+@login_required(login_url='SignIn')
+def hostedevents(request, id):
+    usern = UserAccount.objects.filter(username=request.user).values()[0]['user_id_number']
+    event = Event.objects.get(event_id=id)
+    form = FullEventForm(instance=event)
+    form2 = EventWorkoutForm(instance=event)
+    workouts = EventWorkOut.objects.filter(event=id)
+
+    if request.method == 'POST':
+        if 'workout_name' in request.POST:
+            form2 = EventWorkoutForm(request.POST)
+            if form2.is_valid():
+                form2.save()
+  
+        elif 'event_id' in request.POST:    
+            form = FullEventForm(request.POST, instance=event)
+            if form.is_valid():
+                form.save()
+                return redirect('Home')
+
+
+    return render(request, 'PlanIFYweb/hostevent.html', {'pagename': event, 'form': form, 'form2': form2, 'workout_list': workouts})
